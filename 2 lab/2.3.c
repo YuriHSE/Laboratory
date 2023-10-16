95ef1379380582b8ad2ae9b6ec46958bcb9be5a9
YuriHSE <145991450+YuriHSE@users.noreply.github.com>
2023-10-16 22:12:42 +0300

    Update 2.3.c

--- 2 lab/2.3.c
+++ 2 lab/2.3.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 int function(long double a, long double b){ //function with one complex condition that returns true or false
-    return a >= 0 && (b >= 0 && b <= 1 - 2*a || b < 0 && b >= -2*a - 1) || a < 0 && b < 0 && pow(a, 2) + pow(b, 2) <= 1;
+    return a >= 0 && (b >= 0 && (b < 1 - 2*a || fabs(b - 1 + 2*a) < 0.000001) || (b < 0 && (b > 2*a - 1 || fabs(b - 2*a + 1) < 0.000001))) || (a < 0 && b <= 0 && (pow(a, 2) + pow(b, 2) < 1 || fabs(pow(a, 2) + pow(b, 2) - 1) < 0.000001));
 }
 int main(){
     long double x, y; //defining variables
