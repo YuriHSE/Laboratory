7eebc61575847246b5ec5ad55432d23dbe5df06f
YuriHSE <145991450+YuriHSE@users.noreply.github.com>
2023-10-22 22:23:33 +0300

    Update 3_recursion.c

--- 3 lab/3_recursion.c
+++ 3 lab/3_recursion.c
@@ -1,42 +1,67 @@
 #include <stdio.h>
 #include <math.h>
-int number_sequence(int k){ //calculating denominator and numerator
-    int t = 1;
-    while(k>0){
-        t *= k;
-        k -= 2;
+long double f_while(long double x, int n, int a){ //function for WHILE
+    long double sum = 1;
+    while (n){
+        long long int numerator = 1, denominator = 1;
+        int k = n; //copy n
+        //counting each term
+        while (k>0){
+            numerator *= 2*k - 1;
+            denominator *= 2*k;
+            k--;
+        }
+        long double slag = (long double)numerator / denominator; 
+        sum += pow(-1, n)*pow(x, n) * slag;
+        n--;
     }
-    return t;
-}
-float sum(int n, float x){ //expression counting
-    float num, det;
-    if (n == 0){
-        return 1;
+    /*if (isnan(sum)){
+        printf("The value is NAN");
+    } else if (isinf(sum)){
+        printf("The value is INF");
     } else {
-        num = pow(-1, n) * pow(x, n) * number_sequence(2*n - 1);
-        det = number_sequence(2*n);
-    }
-    return num / det + sum(n-1, x);
+        printf("Result: %.*Lf", a, sum);
+    }*/
+    printf("Result: %.*Lf", a, sum);
+    return 0;
 }
-int main(){
-    float x;
-    int n, accuracy;
-    char a;
-    printf("Enter the value x, n and accuracy: ");
-    while (1){ //the cycle will not be interrupted until incorrect data is entered
-        while(scanf("%f", &x) != 1 || scanf("%d", &n) != 1 || scanf("%d", &accuracy) != 1){ //checking the first characters of the input data
-            printf("Incorrect value, try again: ");
-            while(getchar() != '\n');
+long double f_for(long double x, int n, int a){ //function for FOR 
+    long double sum = 1;
+    for (int i = 1; i < n+1; ++i){
+        long long int slag1 = 1;
+        long long int slag2 = 1;
+        for (int j = 1; j <= i; ++j){
+            slag1 *= 2*j - 1;
+            slag2 *= 2*j;
         }
-        scanf("%c", &a);
-        if (a == '\n'){ //lazy input for remainder of last element
-            break;
-        } else {
-            printf("Incorrect value, try again: ");
-            while(getchar() != '\n');
-            //continue
-        }
+        long double slag = (long double)slag1 / slag2; 
+        sum += pow(-1, i)*pow(x, i) * slag;     
     }
-    printf("Values:\nx = %f \nn = %d\naccuracy = %d\n", x, n, accuracy);
-    printf("%.*f", accuracy, sum(n, x));
+    printf("Result: %.*Lf", a, sum);
+    return 0;
+}
+int main(){ 
+    int p, a, n; //defining variables
+    long double x;
+    printf("Enter the x: ");
+    scanf("%Lf", &x);
+    while (fabs(x) >= 1){ //checking value for a given condition
+        printf("Incorrect entered value, try again: ");
+        while(getchar() != '\n');
+        scanf("%Lf", &x);
+    }
+    while(getchar() != '\n'); //clearing the buffer from the remainder of the entered line
+    printf("Enter the n: ");
+    scanf("%d", &n);
+    printf("Enter the accuracy: ");
+    scanf("%d", &a);
+    printf("How to calculate a function: through a cycle \x1B[1;31mWHILE\x1B[0;37m or through a cycle \x1B[1;31mFOR\x1B[0;37m, write the number 1 or 2 accordingly\nEnter the value: ");
+    scanf("%d", &p);
+    while (p != 1 && p != 2){
+        printf("Error\nTry again: ");
+        while(getchar() != '\n');
+        scanf("%d", &p);
+    }
+    if (p == 1) f_while(x, n, a);
+    if (p == 2) f_for(x, n, a);
 }
